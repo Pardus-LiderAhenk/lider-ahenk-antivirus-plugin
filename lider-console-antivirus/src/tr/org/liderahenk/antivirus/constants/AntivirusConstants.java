33436ef8f9c541344a777d390289a0789ac60278
emrekgn <emre.akkaya@agem.com.tr>
2016-08-10 11:48:22 +0300

    Initial commit

---
+++ lider-console-antivirus/src/tr/org/liderahenk/antivirus/constants/AntivirusConstants.java
@@ -0,0 +1,9 @@
+package tr.org.liderahenk.antivirus.constants;
+
+public class AntivirusConstants {
+	
+	public static final String PLUGIN_NAME = "antivirus";
+	
+	public static final String PLUGIN_VERSION = "1.0.0";
+	
+}
