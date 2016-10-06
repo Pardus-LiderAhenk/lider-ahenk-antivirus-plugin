7908dd9e3cbc9d2b52adf1f355b79e3404700899
cemrealpsoy <cemrealpsoy@gmail.com>
2016-10-06 14:55:20 +0300

    main.py desc added

--- ahenk-antivirus/antivirus/main.py
+++ ahenk-antivirus/antivirus/main.py
@@ -7,7 +7,7 @@ def info():
     inf['name'] = 'antivirus'
     inf['version'] = '1.0.0'
     inf['support'] = 'debian'
-    inf['description'] = ''
-    inf['developer'] = ''
+    inf['description'] = 'Antivirus plugin installs and manages antivirus configuration'
+    inf['developer'] = 'cemre.alpsoy@agem.com.tr'
 
     return inf
