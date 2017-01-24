42da5a81384ada60a9df3716a26c50203e1c2813
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-01-24 16:44:40 +0300

    version name fixed

--- lider-console-antivirus/src/tr/org/liderahenk/antivirus/constants/AntivirusConstants.java
+++ lider-console-antivirus/src/tr/org/liderahenk/antivirus/constants/AntivirusConstants.java
@@ -4,7 +4,7 @@ public class AntivirusConstants {
 	
 	public static final String PLUGIN_NAME = "antivirus";
 	
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-beta";
 	
 	public static final class PARAMETERS {
 		public static final String IS_RUNNING = "isRunning";
